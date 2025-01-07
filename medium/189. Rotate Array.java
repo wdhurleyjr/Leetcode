2f68d02f6731f9bd90935d631727b3d09bee1adc
William Hurley <128835969+wdhurleyjr@users.noreply.github.com>
2025-01-07 13:43:41 -0800

    Update 189. Rotate Array.java
    Added notes to solved Leetcode problem.

--- medium/189. Rotate Array.java
+++ medium/189. Rotate Array.java
@@ -1,3 +1,16 @@
+// Notes:
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// The approach for normalizing k with mod n is that any rotation over the length of the array
+// would be extra work and decrease inefficency. Example: If there is an array of length 7 and
+// k = 9, we can simply 9 % 7 = 2 giving us total of 2 rotations to solve this example. Additionally,
+// we reverse the initial array using a two pointer helper function and then split the array based on
+// k and reverse the two halves and solving the problem. The overall complexity is O(2n) -> O(n). 
+// Lastly, the space complexity is O(1) because this problem doesnt not declare data structure that
+// would grow as nums grows in size and memory. The temp variable is essentially static in this process.
+
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
@@ -7,9 +20,8 @@ class Solution {
         reverse(nums, k, n - 1);
     }
     private void reverse(int[] nums, int i, int j) {
-        int temp;
         while (i < j) {
-            temp = nums[i];
+            int temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
             i++;
