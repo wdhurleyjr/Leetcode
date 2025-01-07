31fb2b509abb74931ee0c00be99999ad9a2e6f8f
William Hurley <wdhurleyjr@gmail.com>
2025-01-07 12:23:00 -0800

    Solution for Leetcode #189. Rotate Array

---
+++ medium/189. Rotate Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    private void reverse(int[] nums, int i, int j) {
+        int temp;
+        while (i < j) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
